1bbce6ed9468fb33fc5f60f616cf59604d45bcb9
Fletcher Kelly <flkelly@microsoft.com>
2021-01-20 16:20:33 +0200

    updated ps1 script to use templateURI

--- LZReview/deployLZWorkbook.ps1
+++ LZReview/deployLZWorkbook.ps1
@@ -1,10 +1,13 @@
 #example 
 #$rgName = "lztest1"
-$rgName = ""
+$rgName = "lztest3"
 
 #example 
 #$resourceName = "Landing Zone Workbook Workbook resource"
-$resourceName = ""
+$resourceName = "Landing Zone Workbook Workbook resource 1"
+
+#example = "https://raw.githubusercontent.com/fskelly/lzreview/main/LZReview/landingzonereview-workbookGalleryTemplatev2.json"
+$templateURI = "https://raw.githubusercontent.com/vanessabruwer/lzreview/main/LZReview/landingzonereview-workbookGalleryTemplatev2.json"
 
 #check to see if Resource Group exists
 $rgExist = Get-AzResourceGroup -Name $RgName -ErrorAction SilentlyContinue
@@ -12,12 +15,12 @@ $rgExist = Get-AzResourceGroup -Name $RgName -ErrorAction SilentlyContinue
 if ($rgExist)
 {
     Write-host "Deploying to $rgName"
-    New-AzResourceGroupDeployment -ResourceGroupName $rgName -TemplateFile .\LZReview2.json -resourceName $resourceName 
+    New-AzResourceGroupDeployment -ResourceGroupName $rgName -TemplateUri $templateURI -resourceName $resourceName 
 }
 else 
 {
     write-host "$rgName does not exist"
     $location = read-host "Which Azure Region should it be deployed to?"
     New-AzResourceGroup -Name $rgName -Location $location
-    #New-AzResourceGroupDeployment -ResourceGroupName $rgName -TemplateFile .\LZReview2.json -resourceName $resourceName 
+    New-AzResourceGroupDeployment -ResourceGroupName $rgName -TemplateUri $templateURI -resourceName $resourceName 
 }
